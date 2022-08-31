73f774568fbea7fb2d6a907a6d57fd275788fcb6
harry <79763184+LiquidInteract@users.noreply.github.com>
2022-09-01 00:00:35 +0100

    Update PrivacyContainer.tsx
    Ive updated the container even though im not sure what im doing

--- src/views/legal/PrivacyContainer.tsx
+++ src/views/legal/PrivacyContainer.tsx
@@ -8,12 +8,57 @@ const TermsContainer: Component = () => {
             <div class="flex items-center justify-center md:space-x-10 h-[24rem]">
                 <p class={'text-9xl font-semibold text-white underline decoration-blue-500'}>Privacy <span class={'text-blue-500'}>Policy</span></p>
             </div>
-            <Banner
-                text={'This page is currently unavailable.'}
-                type={'error'}
-                icon={false}
-                showCloseIcon={false}
-            />
+            <div>
+                <div>
+                    <p>Nero Nodes
+Official Privacy Policy
+Last Updated: 22nd April 2021
+
+GENERAL
+As you access any portion of Nero Nodes, it is of utmost priority for the private information for all of our Users to remain secure. When accessing any publication under the “Nero Nodes” branding you automatically agree to the Privacy Policy stated below.
+
+If you are requiring any clarification pertaining to any portion of the policy outlined below, it is your duty to contact the Management team by opening a ticket in the Discord server.
+
+PERSONAL INFORMATION
+When registering, or purchasing a service from the Nero Nodescompany you are subject to the storage of the information presented at the time. The information collected is not distributed to the public eye in any manner.
+
+The following list of information is subject to being collected; Full Legal Name, Email Address, Account Information, Payment Information, IP Address, and Uploaded Files.
+
+Under no circumstance are you to use information which is not legally yours to avoid any punishment presented to you and/or your account. If you have been given a punishment (i.e blacklist) from Nero Nodes it is for a legitimate reason and allows for us to ensure the safety of others.
+
+If under any circumstance you wish to not be subject to this personal information collection, it is your duty to remain off the website, and Discord Server.
+
+DATA USAGE
+All information collected by the Nero Nodes is all in order to ensure a secure, ease of use system. Under no circumstance will your information be used as a form of monetization, inconsiderate threats, or marketing targets.
+
+With this, if you would like to understand more pertaining to the data collection. You may contact the Management team by opening a ticket in the Discord Server for clarification, and further insurance on the security of the database. 
+
+Ultimately, when using our services it is our goal to ensure you feel secure with allowing us to not only provide you with a service but also securely store your data.
+
+DATA BREACH
+Due to you allowing your information to be securely stored by Nero Nodes, you are responsible for ensuring the information provided stays secure. Under the possibility that the company has a data breach, there will be no allowance for a legal case, or any form of compensation for your troubles that may ensue by those who gained access to your information due to the breach.
+
+You are completely responsible for the information which you provide, and will not be given support in securing the information at any future date.
+
+THIRD-PARTY LINKS
+As Nero Nodes is a company, the possibility of a partnership may arise. With that, there may be links which you click and in result taken to a third-party website. When clicking those links you assume all risk in determining the security of the website and the future of the data which that site may anonymously collect.
+
+When partnering with these third-party companies Nero Nodes does not guarantee the security of these companies or websites. All information you provide them is at your own risk. You should only do so if you feel as though the company has taken all correct security measures.
+
+Under the circumstance your information is leaked through these third-party sites, Nero Nodes will not provide any sort of support in securing your data or contacting the Owner(s) of the third-party site.
+.
+ACCOUNT SECURITY
+With creating an account with Nero Nodes you are responsible for ensuring that your account is only accessed by you and no one else. 
+
+If you provide others access to your account you are subject to having your personal information leaked outside of what would be deemed a Nero Nodes wide data breach. 
+
+Under the circumstance you are looking to have others be able to access your server hosting files you are to do so by adding sub-users to the server. This is to be the only way in which someone other than yourself accesses your server files. 
+
+GDPR REGULATIONS
+With Nero Nodes being located in the United Kingdom it operates under that law of governance. Under no circumstance are you to breach any of the laws outlined in the GDPR Regulations or United Kingdom laws as a whole.
+
+If you are requesting clarification on these laws and regulations you are to do so by opening a ticket in the Discord Server, otherwise, it will be assumed that the information is clear and there is no confusion. Therefore, you will be held completely accountable.</p>
+                </div>
             <div class="flex items-center justify-center md:space-x-10 py-10">
                 <p class={'text-xl font-semibold text-gray-500'}>
                     Last Updated: 7 July 2022
