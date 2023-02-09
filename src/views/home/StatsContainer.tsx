8e42127b2743afee693e810eded55f532bf36071
Cam White <72230943+cmrxnn@users.noreply.github.com>
2023-02-09 09:37:27 +0000

    Update StatsContainer.tsx

--- src/views/home/StatsContainer.tsx
+++ src/views/home/StatsContainer.tsx
@@ -3,19 +3,19 @@ export default () => {
         <div class="max-w-7xl mx-auto px-4 py-[12rem] sm:px-6">
             <dl class="space-y-20 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-20">
                 <div class="relative">
-                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">5000+</p>
+                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">10,000+</p>
                     <dd class="mt-16 ml-16 text-base text-blue-500 text-bold">Total Servers Deployed.</dd>
                 </div>
                 <div class="relative">
-                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">6000+</p>
+                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">12,000+</p>
                     <dd class="mt-16 ml-16 text-base text-blue-500 text-bold">Clients Served.</dd>
                 </div>
                 <div class="relative">
-                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">99.9%</p>
+                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">99.7%</p>
                     <dd class="mt-16 ml-16 text-base text-blue-500 text-bold">Server Uptime.</dd>
                 </div>
                 <div class="relative">
-                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">E5-1650V2</p>
+                    <p class="ml-16 text-[6rem] leading-6 font-medium text-white">9900k</p>
                     <dd class="mt-16 ml-16 text-base text-blue-500 text-bold">Powerful CPUs.</dd>
                 </div>
             </dl>
